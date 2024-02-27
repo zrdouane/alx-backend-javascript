9406d73ebccff2b7a5e9f951c7cbded87e4a98f0
zrdouane <ramirdouane@gmail.com>
2024-02-27 02:27:24 +0100

    fix format

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-function displayMessage (string) {
-  console.log(string);
-}
-
-module.exports = displayMessage;
+function displayMessage(string) {
+    console.log(string);
+  }
+  
+  module.exports = displayMessage;
